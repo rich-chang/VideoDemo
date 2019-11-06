217e2e9b3089a99d03b6b4bf2c32b56a8f1276f2
Rich Chang <rich_chang@outlook.com>
2019-11-06 09:53:57 +0800

    Add demo MediaController bar

--- app/src/main/java/com/richc/videodemo/MainActivity.java
+++ app/src/main/java/com/richc/videodemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.richc.videodemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +14,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         VideoView vv = findViewById(R.id.demoVideoView);
-
+        MediaController mc = new MediaController(this);
+        
+        mc.setAnchorView(vv);
+        vv.setMediaController(mc);
         vv.setVideoPath("android.resource://" + getPackageName() + '/' + R.raw.open_highway_long_road_winter);
 
         vv.start();
