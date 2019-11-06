fefadaa94e406d23ae499debdcde17eda3a193f5
Rich Chang <rich_chang@outlook.com>
2019-11-06 09:51:36 +0800

    Simply start a video by VideoView

--- app/src/main/java/com/richc/videodemo/MainActivity.java
+++ app/src/main/java/com/richc/videodemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.richc.videodemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView vv = findViewById(R.id.demoVideoView);
+
+        vv.setVideoPath("android.resource://" + getPackageName() + '/' + R.raw.open_highway_long_road_winter);
+
+        vv.start();
     }
 }
